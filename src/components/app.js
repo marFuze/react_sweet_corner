ccddb074c267cf5676acb2f337819cb1ca5cc225
Mark Narciso <marknar@hotmail.com>
2019-11-20 20:58:21 -0800

    testing different page components

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import '../assets/css/app.scss';
+import { Route } from 'react-router-dom';
+import About from './about';
+import Contact from './contact';
+import Footer from './footer';
+import Header from './header';
+import Home from './home';
+import Services from './services';
 
 const App = () => (
-    <div>
-        <h1>Sweet Corner</h1>
-    </div>
+   <div className='app'>
+       <div className='container'>
+           <Header/>
+           <Footer/>
+       </div>
+   </div>
 );
 
 export default App;
