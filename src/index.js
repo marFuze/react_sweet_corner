04bbe9e0811891951e30ebced2e50a918e4bd2bd
marFuze <marknar@hotmail.com>
2019-11-28 23:26:25 -0800

    base files added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
+const store = createStore(rootReducer);
+
 
 import App from './components/app';
 
 ReactDOM.render(
-    <Router>
-        <App />
-        </Router>,
+    <Provider store={store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>,
     document.getElementById('root')
 );
