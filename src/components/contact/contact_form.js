c22ebfd62e1275ac6ee1ff02c62627f1b81d651c
marFuze <marknar@hotmail.com>
2019-11-29 17:54:09 -0800

    added contact form

---
+++ src/components/contact/contact_form.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class ContactForm extends React.Component {
+    contactUs(fromValues){
+        console.log('Form Values:', fromValues);
+    }
+
+    render(){
+        const { handleSubmit } = this.props;
+        return (
+            <div><h1>Contact Form</h1>
+
+            <form onSubmit={handleSubmit(this.contactUs)}>
+                <div className='row'>
+                <div className='field'>
+                    <Field name='Name' component='input' type='text' />
+                    <label>Name</label>
+                </div>
+                </div>
+
+                <div className='row'>
+                <div className='field'>
+                    <Field name='Email' component='input' type='text' />
+                    <label>Email</label>
+                </div>
+                </div>
+
+                <div className='row'>
+                <div className='field'>
+                    <Field name='Phone' component='input' type='text' />
+                    <label>Phone</label>
+                </div>
+                </div>
+
+                <div className='row'>
+                <div className='field'>
+                    <Field name='Subject' component='input' type='text' />
+                    <label>Subject</label>
+                </div>
+                </div>
+
+                <div className='row'>
+                <div className='field'>
+                    <Field name='Message' component='input' type='text' />
+                    <label>Message</label>
+                </div>
+                </div>
+
+                <div className='row'>
+                    <button>Send Message</button>
+                </div>
+
+            </form>
+            </div>
+        )
+    }
+}
+
+export default reduxForm({
+    form:'contact-form'
+})(ContactForm);
+
+
+
