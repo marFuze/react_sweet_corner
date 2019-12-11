0b3c0dfda5ae2d3c27225edb500ccd9e2d3d6779
YasahiiSoftware <yasashiisoftware@gmail.com>
2019-12-10 18:25:02 -0800

    completed v2 feature set 2

---
+++ src/components/general/money.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default props => {
+    console.log('money props:', props)
+    const moneyFormat = (props.pennies/100).toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      });
+    return (
+        <h3>{moneyFormat}</h3>
+    )
+}
