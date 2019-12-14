7fe924c7d122cb40e1526b9d34190b7c2b01338a
YasahiiSoftware <yasashiisoftware@gmail.com>
2019-12-13 18:00:40 -0800

    completed v3 feature set 2

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,5 @@
 export default {
+    ADD_ITEM_TO_CART: 'add_item_to_cart',
     CLEAR_PRODUCT_DETAILS: 'clear_product_details',
     GET_ALL_PRODUCTS: 'get_all_products',
     GET_CART_TOTALS: 'get_cart_totals',
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
 import rootReducer from './reducers';
 import thunk from './middleware/thunk';
+
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
 
